2a2d9f6bd5a5cb1f3c064e547a34de5cb36de33c
Andras Vincze <andras.vincze@yahoo.com>
2019-07-23 16:35:58 +0200

    commit to

--- list_of_running_programs.py
+++ list_of_running_programs.py
@@ -2,14 +2,16 @@ import subprocess
 import os
 
 
-def close_this_application(program_name):
+def close_this_application(exe_file):
     # Closes application on Windows
-    os.system("TASKKILL /F /IM {}".format(program_name))
+    os.system("TASKKILL /F /IM {}".format(exe_file))
 
 
-def open_this_application():
-    # Opens application on Windows
-    os.system(r'"C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2019.1.2\bin\idea64.exe"')
+def open_this_application(exe_path=r'"C:\Program Files\JetBrains'
+                                   r'\IntelliJ IDEA Community Edition '
+                                   r'2019.1.2\bin\idea64.exe"'):
+    # Opens application on Windows, default is JAVA IDE
+    subprocess.call(exe_path)
 
 
 def string_of_currently_running_applications():
@@ -18,12 +20,16 @@ def string_of_currently_running_applications():
     output = subprocess.check_output(cmd, shell=False, universal_newlines=True)
     return output
 
-# Close the .exe on Windows.
+
+# Close application on Windows, two examples:
 if 'cmd.exe' in string_of_currently_running_applications():
     close_this_application('cmd.exe')
+if 'Spotify.exe' in string_of_currently_running_applications():
+    close_this_application('Spotify.exe')
 
-#Opens IDE
-open_this_application()
+# Opens any program notepad++ as an example
+open_this_application(r"C:\Program Files (x86)\Notepad++\notepad++.exe")
+
 
 
 
