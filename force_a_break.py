058d01556e62451a3a240eb3a0146c0901684348
Andras Vincze <andras.vincze@yahoo.com>
2019-08-14 15:27:01 +0200

    commit to

--- force_a_break.py
+++ force_a_break.py
@@ -7,9 +7,9 @@ def close_this_application(exe_file):
     os.system("TASKKILL /F /IM {}".format(exe_file))
 
 
-def open_this_application(exe_path=r'"C:\Program Files\JetBrains'
-                                   r'\IntelliJ IDEA Community Edition '
-                                   r'2019.1.2\bin\idea64.exe"'):
+def open_this_application(exe_path= (r'"C:\Program Files\JetBrains\IntelliJ'
+                                     r' IDEA Community Edition '
+                                     r'2019.1.2\bin\idea64.exe"')):
     # Opens application on Windows, default is JAVA IDE
     subprocess.call(exe_path)
 
@@ -29,7 +29,7 @@ if 'Spotify.exe' in currently_running_applications():
 
 # Opens any program notepad++ as an example
 open_this_application(r"C:\Program Files (x86)\Notepad++\notepad++.exe")
-
+#hello
 
 
 
