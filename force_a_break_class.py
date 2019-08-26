601ca36c936deb20fdacf1106c9151812a5b2a7c
Andras Vincze <andras.vincze@yahoo.com>
2019-08-26 14:53:14 +0200

    commit to

---
+++ force_a_break_class.py
@@ -0,0 +1,31 @@
+import subprocess
+
+
+class Application:
+
+    def __init__(self,exe):
+        self.exe = exe
+
+    def close_this_application(self):
+        subprocess.call("TASKKILL /F /IM {}".format(self.exe))
+
+    def open_this_application(self):
+        # command to find the Executable path of the exe
+        command = '''wmic process where "name='{}'" get ExecutablePath'''.format(self.exe)
+        # find the exact path out of multiple possible paths
+        output = subprocess.check_output(command, universal_newlines=True)
+        start_of_the_command = output.find('C')
+        end_of_the_command = output.find('exe')
+        sliced_final_command = slice(start_of_the_command, end_of_the_command + 3)
+        # run the exe with the full path
+        print(output[sliced_final_command])
+        #subprocess.call(output[sliced_final_command])
+
+
+instance = Application('chrome.exe')
+instance.open_this_application()
+
+
+
+
+
